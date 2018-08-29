2fa8bd2ca01fc6d33d10f307de2d1d441efc8439
vlecsei <vlecsei@gmail.com>
2018-08-29 14:07:36 +0200

    Ajout d'une fenetre d'affichage de l'opération et du résulat

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class Main {  
     
     public static void main(String[] args) {
 	
 	Calculatrice add = new Calculatrice('+', 30, 50);
 	add.Calculer();
-	System.out.println(add.toString());
+	JOptionPane.showMessageDialog(null, add.toString(), "Résultat", JOptionPane.YES_OPTION);
 	
 
     }
